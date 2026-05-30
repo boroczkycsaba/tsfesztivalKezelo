39691d6d268ae7edb127229a5cccc6879748837e
Böröczky Csaba <boroczky.csaba@gmail.com>
2026-05-30 17:42:49 +0200

    Felhasználó teszt eset további

--- Test/userService.test.ts
+++ Test/userService.test.ts
@@ -4,7 +4,7 @@ import {EventProgramme} from "../src/models/event";
 import {User} from "../src/models/user";
 
 
-describe('event test 1', () => {
+describe('felhasználó test 1', () => {
     const eventService: EventService = new EventService();
     const eventId :string =eventService.startEvent('Esemény');
     const participiantName :string = 'Résztvevő 1';
@@ -19,5 +19,24 @@ describe('event test 1', () => {
             eventService.findParticipant(findedEvent, "Ilyen nincs")
         }).toThrow('Résztvevő sajnos nem található');
     })
+});
 
+
+describe('felhasználó test 2', () => {
+    const eventService: EventService = new EventService();
+    const eventId :string =eventService.startEvent('Esemény');
+    const participiantName :string = 'Résztvevő 1';
+    const participiantEmail :string = 'resztvevo@resztevevo.com';
+    eventService.registerParticipant(eventId, participiantName, participiantEmail, '00-Résztvevő-1');
+    const findedEvent: EventProgramme = eventService.findEvent(eventId);
+    const findedParticipant :User = eventService.findParticipant(findedEvent, '', participiantEmail);
+    test('Résztvevő email test', () => {
+        expect(findedParticipant.email).toBe(participiantEmail);
+    });
+    eventService.removeParticipant(eventId, '', participiantEmail);
+    test('Résztvevő email test már nincs', () => {
+        expect(() => {
+            eventService.findParticipant(findedEvent, '', participiantEmail)
+        }).toThrow('Résztvevő sajnos nem található');
+    })
 });
